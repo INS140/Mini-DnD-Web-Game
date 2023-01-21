639c707d05c6b27cc31df0e83df4cdd048975b59
INS140 <IsaacNSaettel@gmail.com>
2023-01-21 01:11:17 -0800

    refactor

--- assets/JS/classOptions.js
+++ assets/JS/classOptions.js
@@ -1,33 +1,30 @@
 import game from "./game.js"
 
-class Fighter {
-    constructor(name) {
+class Player {
+    constructor(name, hp, ac, init, atkDmg) {
         this.name = name
-        this.hp = 100
-        this.ac = 15
-        this.init = 11
-        this.atkDmg = 10
+        this.hp = hp
+        this.ac = ac
+        this.init = init
+        this.atkDmg = atkDmg
     }
 }
 
-class Wizard {
+class Fighter extends Player {
     constructor(name) {
-        this.name = name
-        this.hp = 60
-        this.ac = 12
-        this.init = 13
-        this.sp = 20
-        this.atkDmg = 15
+        super(name, 100, 15, 11, 10)
     }
 }
 
-class Paladin {
+class Wizard extends Player {
     constructor(name) {
-        this.name = name
-        this.hp = 80
-        this.ac = 14
-        this.init = 12
-        this.sp = 10
+        super(name, 60, 12, 13, 20, 15)
+    }
+}
+
+class Paladin extends Player {
+    constructor(name) {
+        super(name, 80, 14, 12, 10)
     }
 }
 
